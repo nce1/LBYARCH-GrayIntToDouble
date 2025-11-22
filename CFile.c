514ed32cb74366c579d153b29087722b42a369bd
nce1 <nce.ioclen@gmail.com>
2025-11-22 14:49:12 +0800

    Test Update

--- CFile.c
+++ CFile.c
@@ -2,49 +2,48 @@
 #include <time.h>
 #include <stdlib.h>
 
-extern void imgCvtGrayIntToDouble(int h, int w, int *pixels, double *output);
+extern void imgCvtGrayIntToDouble(long long int totalPixels, long long int *pixels);
 int main(){
 	int h = 100, w = 100;
 	printf("Input height and width: ");
 	scanf("%d", &h);
     scanf("%d", &w);	
 
-    long long totalPixels = (long long)h * w;
-    int *pixels = (int*)malloc(totalPixels * sizeof(int));
-    double *output = (double*)malloc(totalPixels * sizeof(double));
+    FILE *fptr = fopen("ConvertedPixels.txt", "w");
+    long long int totalPixels = (long long int)h * w;
+    long long int* pixels = (long long int*) malloc (totalPixels * sizeof(long long int));
 
-    if (pixels == NULL || output == NULL){
+    if (pixels == NULL){
         printf("Error: Could not allocate enough memory on the heap.\n");
         return 1;
-    }
+    } 
 
     printf("Input pixel values per row:\n");
-    for (int i = 0; i < h; i++){
-        for (int j = 0; j < w; j++){
-            pixels[i * w + j] = 64;
-            if (j > 0)
-                scanf(" ,");
-            scanf("%d",  &pixels[i * w + j]);
-        }
+    for (int i = 0; i < totalPixels; i++){
+        if ((i+1) % w == 0)
+            scanf_s("%lld", &pixels[i]);
+        else
+            scanf_s("%lld, ", &pixels[i]);
     }
 
     clock_t start = clock();
-    imgCvtGrayIntToDouble(h, w, pixels, output);
+    imgCvtGrayIntToDouble(totalPixels, pixels);
     clock_t end = clock();
 
     double totalTime = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Running time: %f seconds\n", totalTime);
-    
+
     printf("Output:\n");
-    
-    for (int i = 0; i < h; i++){
-		for (int j = 0; j < w; j++){
-            printf("%.2f ", ((double*) output)[(i * w) + j]);
+    for (int i = 0; i < totalPixels; i++){
+        double value = ((double*) pixels)[i];
+        //printf("%.2f ", value);
+        fprintf(fptr, "%.2f ", value);
+        if ((i+1) % w == 0){
+            //printf("\n");
+            fprintf(fptr, "\n");
         }
-        printf("\n");
 	} 
-    
+    fclose(fptr);
     free(pixels);
-    free(output);    
     return 0;
 }
--- imgCvtGrayIntToDouble.asm
+++ imgCvtGrayIntToDouble.asm
@@ -8,19 +8,17 @@ default rel
 bits 64
 global imgCvtGrayIntToDouble
 imgCvtGrayIntToDouble:
-    IMUL RDX, RCX
-    XOR RCX, RCX
+    XOR R8, R8
     
     L1:
-    MOV EAX, [R8]
-    CVTSI2SD XMM1, EAX 
+    MOV RAX, [RDX]
+    CVTSI2SD XMM1, RAX 
     DIVSD XMM1, [const]
-    MOVSD [R9], XMM1
-    ADD R8, 4
-    ADD R9, 8
-        
-    INC RCX
-    CMP RCX, RDX
+    MOVSD [RDX], XMM1
+    ADD RDX, 8
+    
+    INC R8
+    CMP RCX, R8
     JNZ L1  
     
     RET
